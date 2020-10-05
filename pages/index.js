ad2c69dde41da73be23c886c208b50752dd4f7a1
Abdelrahman El-Adawy <abdel.eladawy@gmail.com>
2020-10-06 01:28:37 +0200

    clean up

--- pages/index.js
+++ pages/index.js
@@ -20,12 +20,3 @@ export default function Home() {
     </>
   );
 }
-
-export async function getServerSideProps() {
-  // Fetch data from external API
-  const res = await fetch(newsApiURL);
-  const data = await res.json();
-
-  // Pass data to the page via props
-  return { props: { posts: data.articles } };
-}
